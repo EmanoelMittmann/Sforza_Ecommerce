9409c69f59b66946ae3e30e2709e2a7333306b9e
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-07-04 21:14:05 -0300

    Init: create project

---
+++ components/atoms/Icons/IconSearch.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const IconSearch = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 16 16'
+			fill='currentColor'
+			aria-hidden='true'
+			focusable='false'
+			role='img'
+			className='wpds-c-jlBemH '
+		>
+			<path
+				fill='currentColor'
+				fill-rule='evenodd'
+				d='M7 3H2v1h5V3Zm2 9H2v1h7v-1ZM2 7.5h4v1H2v-1Zm9.561 1.126a2.333 2.333 0 1 0-1.596-4.385 2.333 2.333 0 0 0 1.596 4.385Zm2.335-3.332a3.334 3.334 0 0 1-1.585 4.093l1.15 3.16-.94.341-1.155-3.175a3.334 3.334 0 1 1 2.53-4.42Z'
+				clip-rule='evenodd'
+			></path>
+		</svg>
+	)
+}
